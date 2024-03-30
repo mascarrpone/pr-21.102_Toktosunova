e6bc5eac2f944715ab7bb88e96f0f7d23cb8e957
user <user@DESKTOPCHIK>
2024-03-30 11:15:45 +0700

    Добавьте файлы проекта.

---
+++ pr-21.102_Toktosunova_3/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace pr_21._102_Toktosunova_3
+{
+    internal class Helper
+    {
+        public static Entity getContext()
+        {
+            return new Entity();
+        }
+    }
+}
---
+++ pr-21.102_Toktosunova_3/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace pr_21._102_Toktosunova_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DbContext dbContext;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // контекст бд с использованием строки подключения
+            string connectionString = "data source=DESKTOPCHIK\\SQLEXPRESS;initial catalog=RK-pr-21.102-19;integrated security=True;encrypt=True;trustservercertificate=True;MultipleActiveResultSets=True;App=EntityFramework";
+            dbContext = new DbContext(connectionString);
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            string searchQuery = searchTB.Text.Trim();
+
+            var query = dbContext.Discipline.Where(d => d.Name.Contains(searchQuery))
+                                             .OrderByDescending(d => d.Name)
+                                             .ThenBy(d => d.Name)
+                                             .ToList();
+
+            if (!query.Any())
+            {
+                MessageBox.Show("Результаты поиска отсутствуют.", "Поиск",
+                                MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                LoadData.ItemsSource = query;
+            }
+        }
+
+        private void LoadData_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+
+   
+   
+}
+
